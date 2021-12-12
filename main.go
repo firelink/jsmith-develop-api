72c141e7c93035b2ba79e71ca6d4398b665d57d3
firelink <moondrop@jsmith-develop.com>
2021-12-12 03:45:53 -0500

    CORS

--- main.go
+++ main.go
@@ -71,7 +71,7 @@ func main() {
 
 func corsMiddleWare(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Access-Control-Allow-Origin", "*.jsmith-develop.com")
+		w.Header().Add("Access-Control-Allow-Origin", "*")
 		next.ServeHTTP(w, r)
 	})
 }
