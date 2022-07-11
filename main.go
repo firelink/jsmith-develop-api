4a2b7112038144b41b214c227f36f17cb7443450
firelink <moondrop@jsmith-develop.com>
2022-07-11 01:42:25 -0400

    Forgot the : for the port

--- main.go
+++ main.go
@@ -66,7 +66,7 @@ func main() {
 	srv := &http.Server{
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
-		Addr:         "8080",
+		Addr:         ":8080",
 		Handler:      router,
 	}
 
